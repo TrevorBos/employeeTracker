cddf6b45813bc1fae145b85bad1b58007c2bd05c
Trevor Bos <trevor.bos@outlook.com>
2022-07-20 13:57:34 -0400

    Adding initial files, as well as placed some starter code into the index.js file to hopefully guide me along the correct path.

---
+++ index.js
@@ -0,0 +1,28 @@
+// add variables for the npm packages and anything else
+
+// create the connection between mysql and application
+
+// inquirer stuff - Ask the user what they would like to do
+
+// Potentially make into seperate ROUTE files for cleaner code below? Or don't if its too complex.
+
+// View all employees
+
+// View Role
+
+// View Department
+
+// Look up employee
+
+// Look Up role
+
+// Look up Department
+
+// Add employee information
+
+// Add Role information
+
+// Add department information
+
+// Update employee Role information
+
